2952b15ff712f45599058112ce9bcbc4e45a50e7
BlanchardIlela <blancilela@gmail.com>
2026-03-01 17:03:26 +0100

    Dans ce projet nous créons un tableau dynamique dont nous allons recupéré les données depuis la BDD pour afficher dans notre écran, en utilisant aussi le système de pagination.

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <title>Tableau Dynamique</title>
+</head>
+<body>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <td>ID</td>
+                <td>Nom</td>
+                <td>Prix</td>
+                <td>Ville</td>
+                <td>Adresse</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>#1</td>
+                <td>Bien 1</td>
+                <td>10 000 CDF</td>
+                <td>Masina</td>
+                <td>Kinshasa</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
